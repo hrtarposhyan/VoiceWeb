e0986082e15f378629dd7aedf8e95b7e9230b92a
hrtarposhyan <hrtarposhyan@gmail.com>
2020-08-13 18:28:16 +0400

    Add model Voice and service

---
+++ VoiceWeb/VoiceWeb/Models/Voice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoiceWeb.Models
+{
+    public class Voice
+    {
+        public string Url { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
---
+++ VoiceWeb/VoiceWeb/Services/IVoiceService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoiceWeb.Services
+{
+    public interface IVoiceService
+    {
+    }
+}

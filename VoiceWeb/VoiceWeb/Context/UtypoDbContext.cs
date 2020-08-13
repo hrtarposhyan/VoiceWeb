0b43f5ba2bce566f32800d23096cdf7f730fef63
hrtarposhyan <hrtarposhyan@gmail.com>
2020-08-13 18:37:12 +0400

    Add DbContext and connection

---
+++ VoiceWeb/VoiceWeb/Context/UtypoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoiceWeb.Context
+{
+    public class UtypoDbContext:IdentityDbContext
+    {
+        public UtypoDbContext(DbContextOptions<UtypoDbContext> options):base(options)
+        {
+            base.Database.EnsureCreated();
+        }
+    }
+}

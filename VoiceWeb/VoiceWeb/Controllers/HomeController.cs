4ab94b7030d2992c52f44c471ebf07b04a8f0df0
hrtarposhyan <hrtarposhyan@gmail.com>
2020-08-13 18:55:45 +0400

    Add function to upload recording voice in controller and app.js, recorder.js

--- VoiceWeb/VoiceWeb/Controllers/HomeController.cs
+++ VoiceWeb/VoiceWeb/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VoiceWeb.Models;
@@ -12,9 +15,11 @@ namespace VoiceWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _appEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment _appEnvironment)
         {
+            this._appEnvironment = _appEnvironment;
             _logger = logger;
         }
 
@@ -23,9 +28,22 @@ namespace VoiceWeb.Controllers
             return View();
         }
 
+        public IActionResult VoiceRecord(IFormFile file)
+        {
+            using(var filestream=new FileStream(_appEnvironment.WebRootPath + "/Files/" + file.Name, FileMode.Create))
+            {
+                file.CopyTo(filestream);
+            }
+            return RedirectToAction("Index");
+        }
         public IActionResult Privacy()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult UploadVoice(IFormFile recording, string fileName)
+        {
+            return Ok();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
